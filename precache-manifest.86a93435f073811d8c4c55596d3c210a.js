8b3bf0f8884f3ac7c23d903dddad8e8de9948b52
rajharsh81070 <rajharsh81070@gmail.com>
2020-10-09 17:36:04 +0530

    Updates

---
+++ precache-manifest.86a93435f073811d8c4c55596d3c210a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f152bf04bd36ec0eb9acaeb28686b2",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "b800977c40691b4ff07f",
+    "url": "/Shopping-Cart/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "a2663381d94a44f08434",
+    "url": "/Shopping-Cart/static/css/main.1619a13f.chunk.css"
+  },
+  {
+    "revision": "b800977c40691b4ff07f",
+    "url": "/Shopping-Cart/static/js/2.10cad40a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Shopping-Cart/static/js/2.10cad40a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2663381d94a44f08434",
+    "url": "/Shopping-Cart/static/js/main.4fe88105.chunk.js"
+  },
+  {
+    "revision": "dc4ddcf3b8b5b79ccf07",
+    "url": "/Shopping-Cart/static/js/runtime-main.ed685d3e.js"
+  }
+]);
